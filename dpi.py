66311185ec1044cb89f62608493ac50a0a8de093
Zamora0627 <158303332+Zamora0627@users.noreply.github.com>
2024-01-31 09:18:11 -0600

    Version 1

---
+++ dpi.py
@@ -0,0 +1,12 @@
+dpi=(input("ingrese dpi"))
+if len(dpi)==13:
+    print("dpi correcto")
+    print("para presidente puedes votar por partido rojo o partido cafe")
+    presidente=input("ingrese su voto: ")
+    print("para alcalde puedes votar por partido verde, partido amarillo, partido azul o partido morado")
+    alcalde=input("ingrese su voto: ")
+    print("por alcalde voto",alcalde)
+    print("por presidente voto",presidente)
+else:
+    print("dpi incorrecto")
+
