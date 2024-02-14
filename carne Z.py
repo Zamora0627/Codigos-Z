555e8eff7c6be72c63bcb66eda3ae9d7171d59eb
Zamora0627 <158303332+Zamora0627@users.noreply.github.com>
2024-02-14 11:00:51 -0600

    Add files via upload
    carne

---
+++ carne Z.py
@@ -0,0 +1,8 @@
+
+for _ in range(10):
+    carne = input("ingrese carne")
+    if len(carne)==8:
+        nombre = input("ingrese nombre y apellido")
+        print("el usauraio ingresado es valido")
+    else:
+        print("carne invalido")
