499ec4edc33b252b3e18093732d32e6f68192b45
Zamora0627 <158303332+Zamora0627@users.noreply.github.com>
2024-03-06 11:32:06 -0600

    Add files via upload

---
+++ Ejercicio 2 Z.py
@@ -0,0 +1,9 @@
+for i in range(1,11):
+    print(i)
+
+
+
+contador = 0
+while contador < 11:
+  print(contador)
+  contador=contador+1
---
+++ Ejercicio 3 Z.py
@@ -0,0 +1,6 @@
+longitud_cm=float(input("ingrese la longitud"))
+longitud_pulgadas=longitud_cm/2.54
+print(longitud_pulgadas)
+longitud_pies=longitud_pulgadas/12
+print(longitud_pies)
+
