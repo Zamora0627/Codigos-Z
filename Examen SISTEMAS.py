49e79c05e0e4f9b28b4a705d196c771ae0230df7
Zamora0627 <158303332+Zamora0627@users.noreply.github.com>
2024-05-22 12:32:48 -0600

    Create Examen SISTEMAS.py

---
+++ Examen SISTEMAS.py
@@ -0,0 +1,7 @@
+primercantidad= float(input("ingrese Q2.50:"))
+segundonumero= float(input("ingrese Q0.50:"))
+print("resultado de la suma:")
+print(Q2.50+Q0.50)
+hora= float (input("ingrese la hora"))
+print("ingrese la hora:")
+if
